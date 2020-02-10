bdba636cedc7851a9b864592e3edab1ed91dd25f
Daniel Mattox <dmattox10@gmail.com>
2020-02-10 14:39:26 -0500

    Added status messages

--- index.js
+++ index.js
@@ -18,48 +18,82 @@ server.get('/api/users', (req, res) => {
         res.status(200).json(users)
     })
     .catch(err => {
-        res.status(500).json({err: err})
+        res.status(500).json({ errorMessage: "The users information could not be retrieved." })
     })
 })
 
 server.get('/api/users/:id', (req, res) => {
     Users.findById(req.params.id)
     .then(user => {
-        res.status(200).json(user)
+        if (user) {
+            res.status(200).json(user)
+        }
+        else {
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
+        }
     })
     .catch(err => {
-        res.status(500).json({err: err})
+        res.status(500).json({ errorMessage: "The user information could not be retrieved." })
     })
 })
 
 server.delete('/api/users/:id', (req, res) => {
-    Users.remove(req.params.id)
+    users.findById(req.params.id)
     .then(user => {
-        res.status(200).json(user)
+        if (user) {
+            Users.remove(req.params.id)
+            .then(user => {
+                res.status(200).json(user)
+            })
+            .catch(err => {
+                res.status(500).json({ errorMessage: "The user could not be removed" })
+            })
+        }
+        else {
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
+        }
     })
-    .catch(err => {
-        res.status(500).json({err: err})
-    })
+    
 })
 
 server.post('/api/users', (req, res) => {
-    Users.insert(req.body)
-    .then(user => {
-        res.status(201).json(user)
-    })
-    .catch(err => {
-        res.status(500).json({err: err})
-    })
+    if (req.body.name && req.body.bio) {
+        Users.insert(req.body)
+        .then(user => {
+            res.status(201).json(user)
+        })
+        .catch(err => {
+            res.status(500).json({ errorMessage: "There was an error while saving the user to the database" })
+        })
+    }
+    else {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    }
+    
 })
 
 server.put('/api/users/:id', (req, res) => {
-    Users.update(req.params.id, req.body)
+    Users.findById(req.params.id)
     .then(user => {
-        res.status(200).json(user)
-    })
-    .catch(err => {
-        res.status(500).json({err: err})
+        if (user) {
+            if (req.body.name && req.body.bio) {
+                Users.update(req.params.id, req.body)
+                .then(user => {
+                    res.status(200).json(user)
+                })
+                .catch(err => {
+                    res.status(500).json({ errorMessage: "The user information could not be modified." })
+                })
+            }
+            else {
+                res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+            }
+        }
+        else {
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
+        }
     })
 })
 
 server.listen(port, () => console.log(`Everything is awesome on port ${port}`))
+
